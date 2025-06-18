5786162233771134f0fbf703323d7b2a785f1e3f
Shubh00796 <shubhamdongare9696@gmail.com>
2025-06-18 18:00:19 +0530

    initial commit

---
+++ src/main/java/com/FinancialTransactionProcessor/FinancialTransactionProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.FinancialTransactionProcessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialTransactionProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialTransactionProcessorApplication.class, args);
+	}
+
+}
